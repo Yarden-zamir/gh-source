6f89e16187280f0b9e7a4554f2bafedc344b1f52
Yarden zamir <make.charcoal@gmail.com>
2023-03-25 13:16:48 +0300

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,59 @@
+# gh-source
+This is a plugin manager for people who don't like plugin managers. It's a simple shell function that downloads and installs plugins from GitHub. It's designed to be used with [zsh](http://www.zsh.org/), but it should work with any shell that supports functions.
+
+## Installation
+in the terminal
+```sh
+git clone https://github.com/yarden-zamir/gh-source.git
+```
+then in your `.zshrc`   
+```sh
+source ~/location/yarden-zamir/gh-source/gh-source.zsh
+```
+(This is the last time you need to clone shell stuff manually)
+## Problem domains / needs
+1. Fast
+2. Simple
+3. Configuring/installing plugins in one location
+4. Updating plugins
+5. Opinionated - because all of my plugins are hosted on github, I can rely on that to make things easier and faster
+6. Customizable
+
+## Before / After
+As someone who didn't like all the stuff that comes with all the popular pluigin managers and hated how most don't even ofer proper updating system. I usually did the following
+
+---
+in the terminal
+```sh
+git clone https://github.com/hlissner/zsh-autopair.git
+```
+then in my `.zshrc`
+```sh
+source ~/github/zsh-autopair/autopair.zsh
+```
+---
+using gh-source, I don't need to run anything ahead of time, just add this line to my `.zshrc`
+```sh
+gh_source hlissner/zsh-autopair/autopair.zsh
+```
+and it will download and source the plugin for me. 
+
+This is useful for sharing dotfiles, because I don't need to share an instruction manual for what to clone before hand or a seperate script. Better yet it lets me update all my plugins easily with `gh_source --update`
+
+## Help
+```sh
+Usage: prog [options] [plugin] [install_command] [install_location]
+Examples:
+  gh-source owner/repo/script.zsh
+  gh-source owner/repo 'source {}/script.zsh && echo potato'
+  gh-source owner/repo 'source {}/script.zsh && echo potato' /home/user/special_location
+  gh-source --update
+Options:
+  --help: print this help message
+  --update: update all plugins
+  --list: list all plugins
+Arguments:
+  plugin: the plugin to source. If no install_command is passed, it will assume the last segment is the file to source (default install command)
+  install_command: the command to run to install the plugin (default: 'source {}/$(echo "$1" | cut -d'/' -f3-) where {} is replaced by install location. meaning that if your plugin is owner/repo/plug.zsh, it will run 'source $install_location/plug.zsh
+  install_location: the location to install the plugin to (default: $GH_SOURCE_INSTALL_LOCATION/$(basename $install_source))
+```
---
+++ gh-source.zsh
@@ -0,0 +1,63 @@
+gh_source() {
+    [ ! type -p gh >/dev/null] && {
+        echo "gh not found on the system" >&2
+        exit 1
+    }
+    [ -z "$PLUGINS" ] && export PLUGINS=""
+    [ -z "$1" ] || [  "$1" = "--help" ] && {
+        echo "Usage: prog [options] [plugin] [install_command] [install_location]"
+        echo "Examples:"
+        echo "  gh-source owner/repo/script.zsh"
+        echo "  gh-source owner/repo 'source {}/script.zsh && echo potato'"
+        echo "  gh-source owner/repo 'source {}/script.zsh && echo potato' /home/user/special_location"
+        echo "  gh-source --update"
+        echo "Options:"
+        echo "  --help: print this help message"
+        echo "  --update: update all plugins"
+        echo "  --list: list all plugins"
+        echo "Arguments:"
+        echo "  plugin: the plugin to source. If no install_command is passed, it will assume the last segment is the file to source (default install command)"
+        echo "  install_command: the command to run to install the plugin (default: 'source {}/\$(echo \"\$1\" | cut -d'/' -f3-) where {} is replaced by install location. meaning that if your plugin is owner/repo/plug.zsh, it will run 'source \$install_location/plug.zsh"
+        echo "  install_location: the location to install the plugin to (default: \$GH_SOURCE_INSTALL_LOCATION/\$(basename \$install_source))"
+        return
+    }
+    [ "$1" = "--update" ] && {
+        if [ ! -z "$ZSH_VERSION" ]; then
+            old_shwordsplit=$(set -o | grep shwordsplit | awk  '{print $2}')
+            set -o shwordsplit
+        fi
+
+        GH_SOURCE_install_location=${GH_SOURCE_INSTALL_LOCATION:-$HOME/Github}
+
+        for plugin in $PLUGINS; do
+            echo "Updating $plugin"
+
+            install_source=$(echo "$plugin" | cut -d'/' -f1,2)
+            install_command=$(echo "$plugin" | cut -d'/' -f3-)
+            install_location=$GH_SOURCE_install_location/$(basename $install_source)
+            
+            install_command=${install_command//\{\}/$install_location)}
+            [ -d "$install_location" ] && \
+                    git --git-dir $install_location/.git --work-tree $install_location pull && \
+                    git --git-dir $install_location/.git --work-tree $install_location reset --hard --quiet
+        done
+        echo "Done updating"
+        return
+    }
+    [ "$1" = "--list" ] && {
+        echo $PLUGINS | tr ' ' '\n'
+        return
+    }
+    export PLUGINS="$PLUGINS $1"
+    GH_SOURCE_install_location=${GH_SOURCE_INSTALL_LOCATION:-$HOME/Github}
+
+    install_source=$(echo "$1" | cut -d'/' -f1,2)
+    install_command=${2:-"source {}/$(echo "$1" | cut -d'/' -f3-)"}
+    install_location=${3:-$GH_SOURCE_install_location/$(basename $install_source)}
+    
+    install_command=${install_command//\{\}/$install_location}
+    
+    [ -d "$install_location" ] || gh repo clone $install_source $install_location &>/dev/null
+
+    eval $install_command
+}
